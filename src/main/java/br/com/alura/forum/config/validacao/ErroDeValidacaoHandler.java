dd355212d02934bf98a42e9f5978f45ad7517dba
Elton Bezerra <elton.cin@hotmail.com>
2019-07-06 12:16:59 -0300

    Bean Validation

---
+++ src/main/java/br/com/alura/forum/config/validacao/ErroDeValidacaoHandler.java
@@ -0,0 +1,42 @@
+package br.com.alura.forum.config.validacao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+
+@RestControllerAdvice
+public class ErroDeValidacaoHandler {
+
+
+    @Autowired
+    private MessageSource messageSource;
+
+    @ResponseStatus(code = HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorDTO> handle(MethodArgumentNotValidException ex) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        List<ErrorDTO> errors = new ArrayList<>(0);
+
+        fieldErrors
+                .forEach(e -> {
+                    String message = messageSource.getMessage(e, LocaleContextHolder.getLocale());
+                    ErrorDTO err = new ErrorDTO(e.getField(), message);
+                    errors.add(err);
+                });
+        return errors;
+    }
+
+}
